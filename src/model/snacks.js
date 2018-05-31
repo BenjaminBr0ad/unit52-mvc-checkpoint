8f063bbbf055b8e097e299ca84716d7b9b16b2db
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-05-31 15:04:50 -0600

    exported json data

--- src/model/snacks.js
+++ src/model/snacks.js
@@ -1,25 +1,6 @@
 const uuid = require('uuid/v4')
 const model = require('./snacks.json')
 
-[
-  {
-    "name": "chips",
-    "id": "d32336ad-a734-4d32-8732-21543a18dfbc"
-  },
-  {
-    "name": "cheese",
-    "id": "b373072c-80df-4fae-8cfd-e8cfb07a0498"
-  },
-  {
-    "name": "hot dogs",
-    "id": "f2a4879b-7157-45ce-b570-5afe7efb6598"
-  },
-  {
-    "name": "popcorn",
-    "id": "d7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2"
-  }
-]
-
 function getAll () {
   return model
 }
