925ef5a879a7593033518f1e14ea78a0ba4c0632
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-05-30 21:47:59 -0600

    All routes created

--- src/controller/snacks.js
+++ src/controller/snacks.js
@@ -1,23 +1,45 @@
 const model = require('../model/snacks')
 
 function getAll (req, res, next) {
-  
+  const snacks = model.getAll()
+  res.status(200).json({ snacks })
 }
 
 function getOne (req, res, next) {
+  const snack = model.getOne(req.params.id)
 
+  if (snack.errors) {
+    return next({ status: 404, message: `Could not find snack with id: ${req.params.id}`, error: snack.errors })
+  }
+  res.status(200).json({ snack })
 }
 
 function addSnack (req, res, next) {
+  const newSnack = model.addSnack()
 
+  if (newSnack.errors) {
+    return next({ status: 400, message: `Could not create new snack`, error: snack.errors })
+  }
+  res.status(201).json({ newSnack: newSnack})
 }
 
 function updateSnack (req, res, next) {
+  let item = "bottled water"
+  const updatedSnack = model.updateSnack(req.params.id, item)
 
+  if (updatedSnack.errors) {
+    return next({ status: 400, message: `Could not update snack`, error: updatedSnack.errors })
+  }
+  res.status(200).json({ updatedSnack: updatedSnack })
 }
 
 function deleteSnack (req, res, next) {
+  const deletedSnack = model.deleteSnack(req.params.id)
 
+  if (deletedSnack.errors) {
+    return next({ status: 404, message: `Could not delete snack`, error: deletedSnack.errors })
+  }
+  res.status(204).json({ deletedSnack: deletedSnack})
 }
 
 module.exports = {
--- src/model/snacks.js
+++ src/model/snacks.js
@@ -19,23 +19,73 @@ const model = [
 ]
 
 function getAll () {
-
+  return model
 }
 
 function getOne (id) {
+  const errors = []
+  const snack = model.find(snack => snack.id === id)
 
+  let response
+  if (!snack) {
+    errors.push(`Could not find snack with id: ${id}`)
+    response = { errors }
+  } else {
+    response = snack
+  }
+  return response
 }
 
-function addSnack (id, name) {
+function addSnack (id=uuidv4(), name="corn dogs") {
+  const errors = []
 
+  let response = undefined
+  for (let elem of model) {
+    if (elem.id === id) {
+      errors.push(`A snack with that id already exists`)
+      response = { errors }
+    }
+  }
+  if (response === undefined) {
+    const newSnack = { name: name, id: id }
+    model.push(newSnack)
+    response = newSnack
+    return response
+  }
 }
 
 function updateSnack (id, name) {
+  const errors = []
+  const snack = model.find(snack => snack.id === id)
 
+  let response
+  if (!snack) {
+    errors.push(`Could not find a snack with id: ${id}`)
+    response = { errors }
+  } else if (!name) {
+    errors.push(`Please provide a snack name`)
+    response = { errors }
+  } else {
+    snack.name = name
+    response = snack
+  }
+  return response
 }
 
 function deleteSnack (id) {
+  const errors = []
+  const snack = model.find(snack => snack.id === id)
 
+  let response
+  if (!snack) {
+    errors.push(`Could not find a snack with id: ${id}`)
+    response = { errors }
+  } else {
+    const index = model.indexOf(snack)
+    model.splice(index, 1)
+    response = model
+  }
+  return response
 }
 
 module.exports = {
